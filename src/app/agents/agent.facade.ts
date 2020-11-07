51be4155a7f79dda78dfcc8b3a3fffc1509b515f
Edinson Jim Sanchez <edinsonjim@gmail.com>
2020-11-07 20:51:54 +0100

    feat: rxjs challenge solved

--- src/app/agents/agent.facade.ts
+++ src/app/agents/agent.facade.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { exhaustMap, map, tap } from 'rxjs/operators';
+import { AnagraficaMap, Contratto } from '../models/contratto';
 import { ContrattoReport } from '../models/contratto-report';
 import { AgentApi } from './agent.api';
 
@@ -8,6 +10,44 @@ export class AgentFacade {
   constructor(private agentApi: AgentApi) {}
 
   getContrattoReport(contrattoId: number): Observable<ContrattoReport> {
-    return of(null);
+    return this.agentApi.fetchContratto(contrattoId).pipe(
+      exhaustMap((contratto: Contratto) => {
+        const ndgTerzi = contratto.terzi.map((t) => t.ndgAnagrafica);
+        const ndgClienti = [contratto.cliente.ndgAnagrafica];
+        const ndgToFetch = [...ndgClienti, ...ndgTerzi];
+
+        return this.agentApi.fetchAnagrafiche(ndgToFetch).pipe(
+          map((anagraficaMap: AnagraficaMap) => {
+            return {
+              contratto,
+              anagraficaMap,
+            };
+          })
+        );
+      }),
+      map(({ contratto, anagraficaMap }) => {
+        const contrattoReport: ContrattoReport = {
+          id: contratto.id,
+          nome: contratto.nome,
+          cliente: {
+            id: contratto.cliente.id,
+            nome: contratto.cliente.nome,
+            cognome: contratto.cliente.cognome,
+            ndgAnagrafica: contratto.cliente.ndgAnagrafica,
+            anagrafica: anagraficaMap[contratto.cliente.ndgAnagrafica],
+          },
+          terzi: contratto.terzi.map((terzo) => {
+            return {
+              ...terzo,
+              anagrafica: anagraficaMap[terzo.ndgAnagrafica],
+            };
+          }),
+        };
+        return contrattoReport;
+      }),
+      tap((contrattoReport: ContrattoReport) =>
+        console.log('[contrattoReport]', contrattoReport)
+      )
+    );
   }
 }
